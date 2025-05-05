af46e1b8023d7f7c04fca849ae9d2408d3eb9add
kumazawa daisuke <first.one.7@gmail.com>
2025-05-05 21:22:47 +0900

    2122更新

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -19,10 +19,32 @@
       </div>
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" />
+          <input class="form__input--first_name" type="text" name="first_name" placeholder="例:山田" value="{{ old('first_name') }}" />
+          <input class="form__input--last_name" type="text" name="last_name" placeholder="例:太郎" value="{{ old('last_name') }}" />
         </div>
         <div class="form__error">
           @error('name')
+          {{ $message }}
+          @enderror
+        </div>
+      </div>
+    </div>
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">性別</span>
+        <span class="form__label--required">※</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--radio">
+          <input class="form__input-radio-male" type="radio" id="male" name="gender" value="male" checked/>
+          <label for="male">男性</label>
+          <input class="form__input--radio-female" type="radio" id="female" name="gender" value="female" />
+          <label for="female">女性</label>
+          <input class="form__input--radio-other" type="radio" id="other" name="gender" value="other" />
+          <label for="other">その他</label>
+        </div>
+        <div class="form__error">
+          @error('gender')
           {{ $message }}
           @enderror
         </div>
@@ -35,7 +57,7 @@
       </div>
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" />
+          <input class="form__input--email" type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" />
         </div>
         <div class="form__error">
           @error('email')

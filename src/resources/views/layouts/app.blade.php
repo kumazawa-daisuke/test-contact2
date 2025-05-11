3e05edf2ec4ace27f484742393b8cee6dbb10aba
kumazawa daisuke <first.one.7@gmail.com>
2025-05-11 09:12:17 +0900

    練習

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Contact Form</title>
+  <title>FashionablyLate</title>
   <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
   <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
   @yield('css')
@@ -14,9 +14,9 @@
 <body>
   <header class="header">
     <div class="header__inner">
-      <a class="header__logo" href="/">
+      <h1 class="header__logo">
         FashionablyLate
-      </a>
+      </h1>
     </div>
   </header>
 
